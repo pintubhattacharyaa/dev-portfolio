8a0feea73dc9d3e3a6a134db84eda451648e6cb4
pintub <pintu.bhattacharya@think360.ai>
2024-11-14 22:05:15 +0530

    Update: Netlify Timeout Error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+  },
+  swcMinify: true,
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
